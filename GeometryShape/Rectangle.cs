96c26d7ad6769fc74314eca93a664e1c13d8419f
LUNA627 <say.samurai@yandex.ru>
2026-02-28 09:54:08 +0500

    create programm

---
+++ GeometryShape/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            if (width < 0 || height < 0)
+            {
+                throw new ArgumentException("Высота и длина должны быть положительными");
+            }
+
+            Width = width;
+            Height = height;
+        }
+
+        public override double Area() => Width * Height;
+        public override double Perimeter() => 2 * (Width + Height);
+        public override string ToString() => $"Прямоугольник: Ширина - {Width}; Длина - {Height}";
+    }
+}
---
+++ GeometryShape/Triangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace GeometryShape
+{
+    public class Triangle : Shape
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            if (sideA < 0 || sideB < 0 || sideC < 0)
+            {
+                throw new ArgumentException("Стороны не должны быть отрицательными");
+            }
+
+            if (sideA + sideB <= sideC || sideB + sideC <= sideA || sideA + sideC <= sideB)
+            {
+                throw new ArgumentException("Треугольника с данными сторонами не существует");
+            }
+            
+    
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        public override double Area()
+        {
+            double p = Perimeter() / 2;
+            return Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC));
+        }
+
+        public override double Perimeter() => SideA + SideB + SideC;
+
+        public override string ToString() => $"Треугольник: {SideA}; {SideC}; {SideC}";
+    }
+}
